571265c5eacd341c0787c7081cf1539e72a88116
Konstantin Bekurin <kbekurin@bk.ru>
2023-03-13 22:48:13 +0300

    Добавлены модели приложения titles

---
+++ api_yamdb/titles/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)

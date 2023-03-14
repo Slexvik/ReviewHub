5b1873bd7243490be25233b1a6270fabdc6a88f1
Сенченко Алексей <slexvik@gmail.com>
2023-03-14 19:30:03 +0700

    Create user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+USER_ROLE = 'user'
+MODERATOR_ROLE = 'moderator'
+ADMIN_ROLE = 'admin'
+
+CHOICES_ROLE = (
+    (USER_ROLE, 'Пользователь'),
+    (MODERATOR_ROLE, 'Модератор'),
+    (ADMIN_ROLE, 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    """Пользователь."""
+    email = models.EmailField(max_length=254, unique=True)
+    bio = models.TextField(
+        verbose_name='Биография',
+        max_length=512,
+        blank=True,
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=CHOICES_ROLE,
+        default=USER_ROLE,
+        blank=True
+    )
+    confirmation_code = models.CharField(
+        max_length=6,
+        verbose_name='Код подтверждения',
+        blank=True,
+    )
 
 
 class Genre(models.Model):
@@ -24,7 +56,7 @@ class Title(models.Model):
     genre = models.ManyToManyField(Genre, through='GenreTitle')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL,
-        null = True,
+        null=True,
         related_name='titles'
     )
 

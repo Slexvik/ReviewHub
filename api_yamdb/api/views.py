60aeb304640551a85693d6e4c31882014a41301a
Konstantin Bekurin <kbekurin@bk.ru>
2023-03-15 16:08:46 +0300

    Добавлена паджинация

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.pagination import LimitOffsetPagination
 
 from .serializers import (
     CategorySerializer, GenreSerializer,
@@ -12,7 +11,6 @@ class CategoryViewSet(viewsets.ModelViewSet):
     """Вьюсет для модели Category."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    pagination_class = LimitOffsetPagination
 
 
 class GenreViewSet(viewsets.ModelViewSet):

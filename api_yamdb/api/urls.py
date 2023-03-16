453bb4a09dc1e66eb2c99ee8265e11911deda0b0
Сенченко Алексей <slexvik@gmail.com>
2023-03-16 13:38:20 +0700

    Добавлено приложние user

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,14 @@
-from rest_framework import routers
-from django.urls import include, path
+# from rest_framework import routers
+# from django.urls import include, path
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+# from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 
-router = routers.DefaultRouter()
-router.register(r'categories', CategoryViewSet, basename='categories')
-router.register(r'genres', GenreViewSet, basename='genres')
-router.register(r'titles', TitleViewSet, basename='titles')
+# router = routers.DefaultRouter()
+# router.register(r'categories', CategoryViewSet, basename='categories')
+# router.register(r'genres', GenreViewSet, basename='genres')
+# router.register(r'titles', TitleViewSet, basename='titles')
 
-urlpatterns = [
-    path('v1/', include(router.urls)),
-]
+# urlpatterns = [
+#     path('v1/', include(router.urls)),
+# ]

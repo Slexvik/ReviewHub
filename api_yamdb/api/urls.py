ac4b8a33e32c90a99a9d1556a3a8fde74982da9d
Сенченко Алексей <slexvik@gmail.com>
2023-03-18 22:12:29 +0700

    for create new branch

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,11 +3,11 @@ from rest_framework import routers
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet)
-from users.views import UserViewSet, create_token, create_user
+# from users.views import UserViewSet, create_token, create_user
 
 
 v1_router = routers.DefaultRouter()
-v1_router.register('users', UserViewSet)
+# v1_router.register('users', UserViewSet)
 v1_router.register(r'categories', CategoryViewSet, basename='categories')
 v1_router.register(r'genres', GenreViewSet, basename='genres')
 v1_router.register(r'titles', TitleViewSet, basename='titles')
@@ -22,7 +22,7 @@ v1_router.register(
 
 
 urlpatterns = [
-    path('v1/auth/signup/', create_user),
-    path('v1/auth/token/', create_token),
+    # path('v1/auth/signup/', create_user),
+    # path('v1/auth/token/', create_token),
     path('v1/', include(v1_router.urls)),
 ]
